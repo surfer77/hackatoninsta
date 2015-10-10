b3b7bd677a61d687963b8206fbcafba2e41d5f16
Adam Hodara <hodaraadam@gmail.com>
2015-10-10 16:44:48 -0600

    last commit

--- client/views/trials.js
+++ client/views/trials.js
@@ -3,15 +3,33 @@ Template.trials.helpers({
         return InstagramSearchedUsers.find();
     },
 
+    currentUsername:function(){
+        return Meteor.user().services.instagram.username;
+    },
+
 });
 
 Template.trials.events({
     'click #searchIgUser': function() {
         instagramUser = $(".form-control").val();
         getIgUserId = Meteor.call("getIgUserId", instagramUser,  function (error, result) {
-    InstagramSearchedUsers.insert(result);
+         var string = JSON.stringify(result);
+         console.log(string);
 });
 
     },
+
+    "change #otherUser": function(){
+                        $('#myModal').modal({
+                        show: 'false'
+                    });
+    },
+
+/*
+    'click #ads': function() {
+        getInstagramData = Meteor.call("getInstagramData",  function (error, result) {
+    userFollows.insert(result);
+});
+*/
 
 });
--- server/methods.js
+++ server/methods.js
@@ -6,9 +6,10 @@ Meteor.methods({
 
         Future = Npm.require('fibers/future');
         var myFuture = new Future();
+        var accessTokenCurrentUser =    Meteor.user().services.instagram.accessToken;
 
 
-        var url = "https://api.instagram.com/v1/users/search?q=" + instagramUser +"&access_token=2230715033.83882ae.30f41db8da944f63a024e6746be9e9b4&count=2000";
+        var url = "https://api.instagram.com/v1/users/search?q=" + instagramUser +"&access_token=" + accessTokenCurrentUser;
 
         Meteor.http.get(url, function(error, results){
             if(error){
@@ -20,7 +21,33 @@ Meteor.methods({
 
         console.log( myFuture.wait() );
         return myFuture.wait();
-    }
+    },
+
+
+    getInstagramData: function(instagramUser){
+
+        console.log("called!");
+
+        Future = Npm.require('fibers/future');
+        var myFuture = new Future();
+        var accessTokenCurrentUser =    Meteor.user().services.instagram.accessToken;
+
+
+        var url = "https://api.instagram.com/v1/users/18808100/follows?access_token=" + accessTokenCurrentUser + "&count=300";
+
+        Meteor.http.get(url, function(error, results){
+            if(error){
+                myFuture.throw(error);
+            } else {
+                myFuture.return(results);
+            }
+        });
+
+        console.log( myFuture.wait() );
+        return myFuture.wait();
+
+
+    },
 
 });
 
